5ddf1ba8e5041449f0cf476744118bd9bca4db59
avinash-89 <avinashmamidi89@gmail.com>
2022-12-07 04:32:43 +0530

    print array

---
+++ Arrays/01-printArray/solution.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    int arr[5] = {1, 2 ,3, 4, 5};
+    cout<<"Array elements are:";
+    for(auto i: arr) cout<<arr[i]<<" ";
+
+}
